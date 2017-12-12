1a0c034209679ade50d7495b70a5bc922720f04f
Tony Westbrook <tonywestbrook58@mail.com>
2017-12-12 14:44:05 -0600

    using Employees DB

--- database-exercise/Downloads/employees_db/limit_exercises.sql
+++ database-exercise/Downloads/employees_db/limit_exercises.sql
@@ -1,11 +1,13 @@
 use employees;
 
-SELECT  last_name
+SELECT  DISTINCT last_name
 FROM employees
-WHERE last_name LIKE 'z%'
+ORDER BY last_name DESC
 LIMIT 10;
 
 SELECT *
 FROM employees
 WHERE hire_date like '%199%' and birth_date LIKE '%12-25'
-LIMIT 5 OFFSET 50;
+ORDER BY birth_date ASC , hire_date DESC
+LIMIT 5 OFFSET 45;
+

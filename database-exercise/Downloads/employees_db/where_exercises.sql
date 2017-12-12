c4bff15f67700d308f082204f1e5d636597be6c9
Tony Westbrook <tonywestbrook58@mail.com>
2017-12-12 11:37:17 -0600

    using Employees DB

---
+++ database-exercise/Downloads/employees_db/where_exercises.sql
@@ -0,0 +1,41 @@
+use employees;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT *
+FROM employees
+WHERE  last_name LIKE 'E%';
+
+SELECT *
+FROM employees
+WHERE hire_date >= '1990-01-01' and hire_date <= '1999-21-31';
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%12-25';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT *
+FROM employees
+WHERE first_name ='Irena' or first_name = 'Vidya' or first_name = 'Maya';
+
+SELECT *
+FROM employees
+WHERE  last_name LIKE '%E%';
+
+SELECT *
+FROM employees
+WHERE  last_name LIKE 'E%' and last_name LIKE '%E';
+
+SELECT *
+FROM employees
+WHERE hire_date like '%199%' and birth_date LIKE '%12-25';
+
+SELECT *
+FROM employees
+WHERE last_name = '%q%';

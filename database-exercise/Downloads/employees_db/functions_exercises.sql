29eee715983a1c1d81bb475a1f4dc651caad206c
Tony Westbrook <tonywestbrook58@mail.com>
2017-12-13 10:17:42 -0600

    using Employees DB

--- database-exercise/Downloads/employees_db/functions_exercises.sql
+++ database-exercise/Downloads/employees_db/functions_exercises.sql
@@ -1,7 +1,22 @@
 USE employees;
 
 
-SELECT concat('Employees Name = ', first_name ,last_name)
+SELECT concat( first_name,' ' ,last_name)
 FROM employees
 WHERE  last_name LIKE '%E%'
-ORDER BY  emp_no;
+ORDER BY  emp_no desc;
+
+
+SELECT
+datediff(now(), hire_date),
+hire_date,
+first_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
+
+
+
+
+

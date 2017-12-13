b3c5011f3c79d5f4306f76a3f294e3196d735916
Tony Westbrook <tonywestbrook58@mail.com>
2017-12-13 11:40:36 -0600

    using Employees DB exercises

--- database-exercise/Downloads/employees_db/group_by_exercises.sql
+++ database-exercise/Downloads/employees_db/group_by_exercises.sql
@@ -1,81 +1,35 @@
 USE employees;
 
-SELECT
-  first_name,
-  last_name
-FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY last_name, first_name;
+SELECT DISTINCT title from titles
+GROUP BY title;
 
 
-SELECT
-  first_name,
-  last_name
-FROM employees
-WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
 
-SELECT
-  gender,
-  first_name,
-  last_name
-FROM employees
-WHERE (first_name = 'Irena'
-       OR first_name = 'Vidya'
-       OR first_name = 'Maya')
-      AND gender = 'M';
-
-SELECT last_name
+SELECT DISTINCT last_name
 FROM employees
 WHERE last_name LIKE 'e%'
-ORDER BY emp_no DESC;
+GROUP BY last_name;
 
-SELECT  last_name
-FROM employees
-WHERE last_name LIKE 'e%' OR last_name LIKE '%e'
-ORDER BY emp_no DESC;
 
-SELECT first_name, last_name
+
+SELECT DISTINCT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'e%' AND last_name LIKE '%e'
 GROUP BY last_name, first_name;
 
 
-SELECT
-  hire_date,
-  first_name
-FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
-
-SELECT
-  hire_date,
-  first_name
-FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-      AND birth_date LIKE '%-12-25'
-ORDER BY birth_date, hire_date DESC;
-
-SELECT
-  hire_date,
-  first_name
-FROM employees
-WHERE hire_date LIKE '199%';
-
-SELECT
-  birth_date,
-  first_name
-FROM employees
-WHERE birth_date LIKE '%-12-25';
-
-SELECT last_name
+SELECT DISTINCT last_name
 FROM employees
 WHERE last_name LIKE '%q%';
 
-SELECT last_name
+SELECT DISTINCT last_name
 FROM employees
 WHERE last_name LIKE '%q%'
       AND last_name NOT LIKE '%qu%'
 GROUP BY  last_name;
 
+SELECT  last_name, count(last_name)
+FROM employees
+WHERE  last_name NOT LIKE  '%z%'
+Group by last_name;
 
-select DISTINCT title from titles
-GROUP BY title;
